f914ae3cd5bc035096e48dcd0b946deed14ae8dd
Ranjan Barman <ranjanbarmann01@gmail.com>
2026-02-08 12:06:10 +0530

    first website commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const noBtn = document.getElementById("noBtn");
+const text = document.getElementById("text");
+
+const funny = [
+  "Please no 😭",
+  "Soch lo 😜",
+  "Dil toot jayega 💔",
+  "Itna bhi bura nahi hoon 🥺"
+];
+
+noBtn.addEventListener("mouseover", () => {
+  noBtn.style.position = "relative";
+  noBtn.style.left = Math.random()*150 + "px";
+  noBtn.style.top = Math.random()*80 + "px";
+
+  text.innerText = funny[Math.floor(Math.random()*funny.length)];
+});
+
+function goYes(){
+  window.location.href = "yes.html";
+}
